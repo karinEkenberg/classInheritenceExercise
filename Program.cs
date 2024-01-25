3cc5d60fa7fd98b7d2b5d6e3c87318b47c1a03de
karinEkenberg <zirkon6@hotmail.com>
2024-01-25 12:16:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Exempel 1. Skriv en konsolapplikation för ett exempel på klass arv.
+ */
+namespace klass_arv
+{
+    internal class Program
+    {
+        public class ExampleClass
+        {
+            public string example;
+            public ExampleClass()
+            {
+                this.example = example;
+            }
+
+            public void Method()
+            {
+                Console.WriteLine("Hello there!");
+            }
+        }
+
+        public class  ExampleInheritance : ExampleClass
+        {
+            public string inheritence;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
